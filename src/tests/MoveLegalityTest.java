230edfeb2ba09e6d4e5f1b26d6fe33fbc8c00a91
Aaron Hammer <athammer@buffalo.edu>
2016-10-17 14:03:46 -0400

    Updated user stories and added more information regarding them, provided us to our great and glorous leader Mr. Hertz

---
+++ src/tests/MoveLegalityTest.java
@@ -0,0 +1,27 @@
+package tests;
+
+import org.junit.Test;
+
+/*
+* Test that your project correctly identifies as legal a move that only goes horizontally for as many squares as the die roll;
+
+* Test that your project correctly identifies as legal a move that only goes vertically for as many squares as the die roll;
+
+* Test that your project correctly identifies as legal a move that goes horizontally & vertically for as many squares as the die roll;
+
+* Test that your project correctly identifies as legal a move that goes through a door and into a room;
+
+* Test that your project correctly identifies as legal a move that just uses the a secret passageway;
+
+* Test that your project correctly identifies as illegal a move that goes more squares than the die roll;
+
+* Test that your project correctly identifies as illegal a move that goes diagonally;
+
+* Test that your project correctly identifies as illegal a move that is not contiguous;
+
+* Test that your project correctly identifies as illegal a move that goes through a wall.
+ */
+
+public class MoveLegalityTest {
+
+}
---
+++ src/tests/SuggestionProofTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.junit.Test;
+
+/*
+
+* Suggestion would be answered by the next player because they have the Player card;
+
+* Suggestion would be answered by the next player because they have the Room card;
+
+* Suggestion would be answered by the next player because they have the Weapon card;
+
+* Suggestion would be answered by the next player because they have 2 matching cards;
+
+* Suggestion would be answered by the player after the next player because they have 1 or more matching cards;
+
+* Suggestion would be answered by the player immediately before player making suggestion because they have 1 or more matching cards;
+
+* Suggestion cannot be answered by any player but the player making the suggestion has 1 or more matching cards;
+
+* Suggestion cannot be answered by any player and the player making the suggestion does not have any matching cards.
+
+*/
+
+
+public class SuggestionProofTest {
+
+}
+
+
