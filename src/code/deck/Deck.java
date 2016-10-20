369f3686a05b14340bb83322e7f6c83d82321f66
Garrett <gowalbor@buffalo.edu>
2016-10-20 18:23:36 -0400

    Deck Class work

--- src/code/deck/Deck.java
+++ src/code/deck/Deck.java
@@ -1,14 +1,44 @@
 package code.deck;
 
+import java.lang.Math; 
+
 public class Deck {
-	String[] _CharacterCards;
-	String[] _WeaponCards;
-	String[] _RoomCards;
+	String[] _CCards;
+	String[] _WCards;
+	String[] _RCards;
+	String[] _Center;
 	
 	String[] _Deck;
 
 	String _Murderer;
 	String _Weapon;
 	String _SceneOfCrime;
+	
+	
+	
+	_CCards = new ArrayList(); 
+	_CCards.add("Prof. Plum"); _CCards.add("Colonel Mustard"); _CCards.add("Mr. Green"); 
+	_CCards.add("Mrs. Peacock"); _CCards.add("Ms. Scarlet"); _CCards.add("Mrs. White");
+	
+	_WCards = new ArrayList();
+	_WCards.add("Rope"); _WCards.add("Lead Pipe"); _WCards.add("Knife"); _WCards.add("Wrench");
+	 _WCards.add("Candlestick"); _WCards.add("Pistol");
+	 
+	 _RCards = new ArrayList(); 
+	 _WCards.add("Ballroom"); _WCards.add("Conservatory"); _WCards.add("Billiard Room");
+	 _WCards.add("Library"); _WCards.add("Study"); _WCards.add("Hall");
+	 _WCards.add("Lounge"); _WCards.add("Dining Room"); _WCards.add("Kitchen");
+	
+	 _Murderer = _CCards.remove(Math.random() * 5 + 1);
+	 
+	 _Weapon = _WCards.remove(Math.random() * 5 + 1);
+	 
+	 _SceneofCrime = _RCards.remove(Math.random() * 5 + 1);
+	 
+	 _Center.add(_Murderer, _Weapon, _SceneOfCrime); 
+	 
+	
+	
+}
 	
 }
