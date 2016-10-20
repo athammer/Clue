c7e8001cce5c451891664ad15b2033ea59bdd169
Garrett <gowalbor@buffalo.edu>
2016-10-20 19:22:37 -0400

    Double-checking changes to Deck class, small edits to Deck class for submission compatibility.

--- src/code/deck/Deck.java
+++ src/code/deck/Deck.java
@@ -16,29 +16,29 @@ public class Deck {
 	
 	
 	
-	_CCards = new ArrayList(); 
-	_CCards.add("Prof. Plum"); _CCards.add("Colonel Mustard"); _CCards.add("Mr. Green"); 
-	_CCards.add("Mrs. Peacock"); _CCards.add("Ms. Scarlet"); _CCards.add("Mrs. White");
+	//_CCards = new ArrayList(); 
+	//_CCards.add("Prof. Plum"); _CCards.add("Colonel Mustard"); _CCards.add("Mr. Green"); 
+	//_CCards.add("Mrs. Peacock"); _CCards.add("Ms. Scarlet"); _CCards.add("Mrs. White");
 	
-	_WCards = new ArrayList();
-	_WCards.add("Rope"); _WCards.add("Lead Pipe"); _WCards.add("Knife"); _WCards.add("Wrench");
-	 _WCards.add("Candlestick"); _WCards.add("Pistol");
+	//_WCards = new ArrayList();
+	//_WCards.add("Rope"); _WCards.add("Lead Pipe"); _WCards.add("Knife"); _WCards.add("Wrench");
+	//_WCards.add("Candlestick"); _WCards.add("Pistol");
 	 
-	 _RCards = new ArrayList(); 
-	 _WCards.add("Ballroom"); _WCards.add("Conservatory"); _WCards.add("Billiard Room");
-	 _WCards.add("Library"); _WCards.add("Study"); _WCards.add("Hall");
-	 _WCards.add("Lounge"); _WCards.add("Dining Room"); _WCards.add("Kitchen");
+	// _RCards = new ArrayList(); 
+	// _WCards.add("Ballroom"); _WCards.add("Conservatory"); _WCards.add("Billiard Room");
+	// _WCards.add("Library"); _WCards.add("Study"); _WCards.add("Hall");
+	// _WCards.add("Lounge"); _WCards.add("Dining Room"); _WCards.add("Kitchen");
 	
-	 _Murderer = _CCards.remove(Math.random() * 5 + 1);
+	// _Murderer = _CCards.remove(Math.random() * 5 + 1);
 	 
-	 _Weapon = _WCards.remove(Math.random() * 5 + 1);
+	// _Weapon = _WCards.remove(Math.random() * 5 + 1);
 	 
-	 _SceneofCrime = _RCards.remove(Math.random() * 5 + 1);
+	// _SceneofCrime = _RCards.remove(Math.random() * 5 + 1);
 	 
-	 _Center.add(_Murderer, _Weapon, _SceneOfCrime); 
+	// _Center.add(_Murderer, _Weapon, _SceneOfCrime); 
 	 
 	
 	
-}
+
 	
 }
