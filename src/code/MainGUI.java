59551c0becc1509cc1e8340f02cddd0ed94325f3
Aaron Hammer <athammer@buffalo.edu>
2016-11-11 14:34:28 -0500

    Added basic gu iand testing main class to help open it.

---
+++ src/code/MainGUI.java
@@ -0,0 +1,11 @@
+package code;
+
+import code.gui.GUI;
+
+public class MainGUI {
+	//only for testing gui
+	public static void main(String[] args) throws Exception{
+		GUI gui = new GUI();
+	}
+
+}
--- src/code/gui/GUI.java
+++ src/code/gui/GUI.java
@@ -1,8 +1,17 @@
 package code.gui;
 
+import java.awt.*;
+
+import javax.*;
+import javax.swing.JFrame;
+
 public class GUI {
 	public GUI(){
-		//start here
+		JFrame f = new JFrame("Clue - Aaron H, Zee, Adam S, Garret");  
+		f.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+	    f.setLayout(null);  
+	    f.setVisible(true);  
+	    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
 		
 	}
 
