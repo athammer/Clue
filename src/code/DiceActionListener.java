a31add47e7fbbacf01b2b34f547b4597e8e66022
xenagonz <xenagonz@buffslo.edu>
2016-11-17 14:19:53 -0500

    GUI

--- src/code/DiceActionListener.java
+++ src/code/DiceActionListener.java
@@ -1,5 +1,14 @@
 package code;
 
-public class DiceActionListener {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DiceActionListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
